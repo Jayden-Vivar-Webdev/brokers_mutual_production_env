6152bfbd904286de68389099bec5d881298bcea6
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2024-12-20 13:06:07 +1100

    Tail wind javascript configuration file added, All pages added, links to pages added for nav bar and other clickable items, navbar fixed, custom styles for columns added, images, output.css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"], // Looks for .html and .js files in the same folder
+  theme: {
+    extend: {
+    },
+  },
+
+}
