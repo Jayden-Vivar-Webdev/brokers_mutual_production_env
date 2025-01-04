61301c06422de9d8d1990e847a52f6f000493f5b
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-01-04 22:28:09 +1100

    animation fix

--- scripts/app.js
+++ scripts/app.js
@@ -8,15 +8,20 @@ export function toggleVisibility(toggleClass, toggleId, toggleVisible = "hidden"
     });
 }
 
-//Appearing elements function 
-export const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-});
+export const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    },
+    {
+        threshold: 0.1, // Element appears when 10% of it is visible
+    }
+);
+
 
 
 
